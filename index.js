8e9da2014db65cd5a0ba462e7083f59d26ec1165
Maria Burlando <maria.bu62442@gmail.com>
2018-04-17 19:17:15 +0200

    Major refactoring of code

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
+var bp = require('body-parser');
+var request = require('request');
 
 var app = express();
 app.use(express.static(path.join(__dirname, '/static')));
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 app.get('/', (req,res) => {
-
 	res.render('home');
 });
 
 app.get('/about', (req,res) => {
-	res.render('about', {about:'this is mvburlando.cloud about page'});
+	res.render('about');
 });
 
 app.listen(3000);
