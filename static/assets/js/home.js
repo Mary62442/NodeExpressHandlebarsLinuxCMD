479a0e09d4b10e1a01ed550fe87ffaf9a2880dfb
Maria Burlando <maria.bu62442@gmail.com>
2018-04-19 15:12:48 +0200

    Created handwriting presentation and fixed responsive design

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -1,3 +1,24 @@
+$(document).ready(()=> {
+    for (var i = 1; i <= 34; i++) {
+        $(".hand-writing").append(`<div id = "mvb${i}" style="background-image: url('assets/images/hand-writing/mvb${i}.png')"></div>`);
+    }
+
+    setTimeout( () => {
+        var counter = 34;
+        var timer = setInterval( () => { 
+            $(".hand-writing > div").hide();
+            $(`#mvb${counter}`).show();
+            counter--;  
+            if (counter === 0)   {
+                clearInterval(timer); 
+                $("#home-all").show();  
+                $(".hand-writing").delay(500).fadeOut(800);
+            }         
+        }, 70);
+    }, 500);
+});
+
+
 $( window ).scroll(() => {
     let offsetT = $('#quote').offset().top;
     let innerH = window.innerHeight;
@@ -18,7 +39,7 @@ $('#change-quote').on('click', (e) => {
       url: '/newquote',
       success: (data) => {  
         $("#quote").css({"opacity": "1", "margin-bottom":"0"});        
-        $('#quoter').text(data.quoter);
+        $('#quoter').html(data.quoter);
         $('#blockquote').html(data.quote);
       },
       cache: false
