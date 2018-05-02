368b2d8c4cabfd4968430ff912748db8a61b3e26
Maria Burlando <maria.bu62442@gmail.com>
2018-05-02 10:45:38 +0200

    Initiated tmdb data visualisation in angular 1.6.9

---
+++ static/app/services/tmdbservice.js
@@ -0,0 +1,46 @@
+app.service('tmdb', ['$http', function($http)  { 
+
+    var defaultMovie_ = function() {
+
+        return $http({
+            url: 'https://api.themoviedb.org/3/search/movie?', 
+            method: "GET",
+            params: {
+                api_key: '37267a52fc129ad7d6ddb14573fb0009',
+                query: 'interstellar'
+            }
+        })        
+        .then((data) => { 
+        return data; 
+        }) 
+        .catch((err) => { 
+        return err; 
+        }); 
+
+    }
+
+    var searchMovie_ = function(movie) {
+        return $http({
+            url: 'https://api.themoviedb.org/3/search/movie?', 
+            method: "GET",
+            params: {
+                api_key: '37267a52fc129ad7d6ddb14573fb0009',
+                query: movie
+            }
+        })        
+        .then((data) => { 
+        return data; 
+        }) 
+        .catch((err) => { 
+        return err; 
+        }); 
+    }
+
+    return {
+        defaultMovie: defaultMovie_,
+        searchMovie: searchMovie_
+    }
+    
+  }]);
+
+  
